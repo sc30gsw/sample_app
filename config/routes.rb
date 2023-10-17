a2f36a4df89502b789af6ad4b55424937e7e49de
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-17 12:58:41 +0900

    リスト3.42の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
   get "up" => "rails/health#show", as: :rails_health_check
-
-  root "application#hello"
 end
