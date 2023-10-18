67b8df4f6fb5aae9f7e90be9fae3a3f170a2cd70
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-18 13:33:50 +0900

    5.4.2演習No.2コメントアウト解除

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,7 +4,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     # 5.4.1演習No.1
     # 5.4.2演習No.2 コメントアウト
-    # get signup_path
+    # 5.4.2演習No.2 コメントアウト解除
+    get signup_path
     assert_response :success
   end
 end
