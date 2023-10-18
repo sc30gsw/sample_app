c3f48aa4f16388ae21f05a61f42252773e292ca4
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-18 13:21:05 +0900

    5.4.1演習No.1の実装

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    # 5.4.1演習No.1
+    get signup_path
     assert_response :success
   end
 end
