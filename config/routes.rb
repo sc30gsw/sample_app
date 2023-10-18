2d51529e521078b1876cb5f3d0d54123a0a7ce38
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-18 12:16:20 +0900

    5.3.2演習No.1の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # 3.4.4演習No.2_コメントアウト解除
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
+  # 5.3.2演習No.1
+  get  '/help',    to: 'static_pages#help', as: 'helf'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get 'up' => 'rails/health#show', as: :rails_health_check
