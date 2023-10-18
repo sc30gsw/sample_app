85234fd2fa96dc9d508bdf6baac67f35fb82438c
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-18 10:18:06 +0900

    4.4.5演習No.1の実装

--- example_user.rb
+++ example_user.rb
@@ -1,12 +1,21 @@
 class User
-  attr_accessor :name, :email
+  # 4.4.5演習No.1
+  attr_accessor :first_name, :last_name, :email
 
   def initialize(attributes = {})
-    @name  = attributes[:name]
+    # 4.4.5演習No.1 
+    @first_name  = attributes[:first_name]
+    @last_name = attributes[:last_name]
     @email = attributes[:email]
   end
 
+  # 4.4.5演習No.1
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+
   def formatted_email
-    "#{@name} <#{@email}>"
+    # 4.4.5演習No.1
+    "#{full_name} <#{@email}>"
   end
 end
