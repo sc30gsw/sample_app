472db884d2b20267596387dee7e143fa3b60b334
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-19 12:11:55 +0900

    7.3.4演習No.1の実装

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,5 +11,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+
+    # 7.3.4演習No.1
+    assert_select 'li', "Name can't be blank"
+    assert_select 'li', 'Email is invalid'
+    assert_select 'li', "Password confirmation doesn't match Password"
+    assert_select 'li', 'Password is too short (minimum is 6 characters)'
+
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "a" * 51,
+                                         email: "a" * 244 + "@example.com",
+                                         password:              "",
+                                         password_confirmation: "" } }
+    end
+    assert_template 'users/new'
+    assert_select 'li', "Name is too long (maximum is 50 characters)"
+    assert_select 'li', 'Email is too long (maximum is 255 characters)'
+    # has_secure_passwordとpasswordのpresence :trueがあるため同じエラーメッセージが出力される
+    assert_select 'li', "Password can't be blank"
+    assert_select 'li', "Password can't be blank"
+    assert_select 'li', 'Password is too short (minimum is 6 characters)'
   end
 end
