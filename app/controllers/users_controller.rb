6ec0a30aaf2dea4ed943bcc6fbf5c6b8292a26aa
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-19 10:55:39 +0900

    リスト7.18の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,11 @@ class UsersController < ApplicationController
   def new
     # 7.1.3演習No.2
     # debugger
-    @user = User.new
+    @user = User.new(params[:user])
+    if @user.save
+      # 保存の成功
+    else
+      render 'new'
+    end
   end
 end
