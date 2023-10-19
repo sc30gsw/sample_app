b541ef108ceafc55c9c3a17534549a92e461353d
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-19 11:05:24 +0900

    リスト7.19の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
     # 7.1.3演習No.2
     # debugger
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       # 保存の成功
     else
       render 'new'
     end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
