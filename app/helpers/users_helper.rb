81194f5d7ad4f915de1fe63ee39edfa0b88160fe
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-19 10:28:58 +0900

    7.1.4演習No.3の実装

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,12 @@ module UsersHelper
 
   # 引数で与えられたユーザーのGravatar画像を返す
   # 7.1.4演習No.2
-  def gravatar_for(user, options = { size: 80 })
+  # def gravatar_for(user, options = { size: 80 })
+  # 7.1.4演習No.3
+  def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
+    # 7.1.4演習No.3
+    # size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
