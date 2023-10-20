09a191f3355e2fcbb2c4894b5fbc622160e19d9a
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-20 18:06:22 +0900

    リスト9.33の実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,8 +17,6 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.permanent.signed[:user_id])
-      # テストがパスすれば、この部分がテストされていないことがわかる
-      raise
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies.permanent[:remember_token])
         log_in user
