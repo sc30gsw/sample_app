ca3f650a3833a350f522982b6601cd3af1e23cbb
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-20 18:03:16 +0900

    演習前の状態に戻す

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,23 @@ class SessionsController < ApplicationController
 
   def create
     # 9.3.1演習No.1
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
+    # @user = User.find_by(email: params[:session][:email].downcase)
+    # if @user && @user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      # log_in @user
+      # params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      # redirect_to @user
+    # else
+      # エラーメッセージを作成する
+      # flash.now[:danger] = 'Invalid email/password combination'
+      # render 'new'
+    # end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
