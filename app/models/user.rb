a64c608a12891c29031a32d14ff57181671f52a6
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-20 11:48:55 +0900

    インデントを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,8 +53,8 @@ class User < ApplicationRecord
   #   end
   # end
 
-   # 渡された文字列のハッシュ値を返す
-   def User.digest(string)
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
