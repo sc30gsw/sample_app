4b6d1a24e7c740461f4ac3ac0207c6da29f2d683
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-20 17:19:36 +0900

    9.1.4演習No.3の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,6 +74,8 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     # 9.1.4演習No.2
     # return false if remember_digest.nil?
+    # 9.1.4演習No.3
+    return false if remember_digest.nil?
 
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
