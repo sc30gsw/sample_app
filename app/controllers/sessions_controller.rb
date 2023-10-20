36348bd9839453bfda9ac523297d11638f9541cf
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-20 17:02:40 +0900

    9.1.4演習No.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out if logged_in?
+    # 9.1.4演習No.1
+    # log_out if logged_in?
+    log_out
     redirect_to root_url
   end
 end
