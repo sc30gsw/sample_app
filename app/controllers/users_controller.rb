a23a3ee3f52acf139d7896297ae475cd82d02564
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-23 11:10:00 +0900

    update_attributesをupdateに修正（rails7対応）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
+    if @user.update(user_params)
       # 更新に成功した場合
     else
       render 'edit'
