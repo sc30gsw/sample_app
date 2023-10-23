a21569c931872c94c94d701699b61fc1bb4926c6
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-23 17:51:50 +0900

    10.2.3演習No.1の実装

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -23,8 +23,12 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    # 10.2.3演習No.1
+    assert_equal session[:forwarding_url], edit_user_url(@user)
     log_in_as(@user)
-    assert_redirected_to edit_user_url(@user)
+    # assert_redirected_to edit_user_url(@user)
+    # 10.2.3演習No.1
+    assert_nil session[:forwarding_url]
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
