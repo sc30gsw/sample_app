5db5a11a0bfe083d00b29deed2c3747cd7b272d2
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-23 18:00:48 +0900

    リスト10.38の実装

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,10 +4,10 @@ module UsersHelper
   # 7.1.4演習No.2
   # def gravatar_for(user, options = { size: 80 })
   # 7.1.4演習No.3
-  def gravatar_for(user, size: 80)
+  def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     # 7.1.4演習No.3
-    # size = options[:size]
+    size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
