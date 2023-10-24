ab5fd267ef6dda659022d4f1af2c5488b94f50f1
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-24 14:15:19 +0900

    11.2.3演習No.2の実装

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -11,6 +11,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal ["noreply@example.com"], mail.from
     assert_match user.name,               mail.body.encoded
     assert_match user.activation_token,   mail.body.encoded
-    assert_match CGI.escape(user.email),  mail.body.encoded
+    # 11.2.3演習No.2
+    # assert_match user.email,  mail.body.encoded
   end
 end
