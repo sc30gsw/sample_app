5de701ebc37829635ce148baa969ed497b39e801
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-24 11:02:08 +0900

    10.4.3演習No.1の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  # 10.4.3演習No.1
+  # before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
