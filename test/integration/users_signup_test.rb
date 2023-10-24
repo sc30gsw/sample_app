5e7db1e16ff89ad3f045c1c0354463a9776a187e
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-24 14:22:49 +0900

    リスト11.24の実装

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -47,9 +47,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
+    # assert_template 'users/show'
     # 7.4.4演習No.1
     assert_not flash.blank?
-    assert is_logged_in?
+    # assert is_logged_in?
   end
 end
