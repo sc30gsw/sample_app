45fa56ea96f061263f98bc3a94cd0139826c5a33
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-25 17:59:55 +0900

    リスト13.11の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts
   attr_accessor :remember_token, :activation_token, :reset_token
   # 6.2.5演習No.2
   # before_save { self.email = email.downcase! }
