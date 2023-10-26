62253e69b89dfc0dc7a7313e9c3ac80ca61b7341
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-26 10:39:17 +0900

    リスト13.65の実装

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,17 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
+  validate  :picture_size
   # 13.3.5演習No.1 無効な送信
   # validates :content, presence: true, length: { maximum: 140 }
   validates :content, presence: true, length: { maximum: 140 }
+
+  private
+
+  # アップロードされた画像のサイズをバリデーションする
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
