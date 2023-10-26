e1b208602c0ff79ff47c5e5e73def5cd97688b20
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-26 10:25:58 +0900

    リスト13.59の実装

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   # 13.3.5演習No.1 無効な送信
   # validates :content, presence: true, length: { maximum: 140 }
