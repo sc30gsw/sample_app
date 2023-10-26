7f950d46127a7a40ad0e13da040e335303e012c3
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-27 06:00:12 +0900

    リスト14.8の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token, :activation_token, :reset_token
   # 6.2.5演習No.2
   # before_save { self.email = email.downcase! }
