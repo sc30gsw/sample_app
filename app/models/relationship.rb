e55dfd442c11896845e670b6eff371f1eede562f
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-27 05:58:48 +0900

    4.1.3演習No.1の実装

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,7 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
-  validates :follower_id, presence: true
-  validates :followed_id, presence: true
+  # 4.1.3演習No.1
+  # validates :follower_id, presence: true
+  # validates :followed_id, presence: true
 end
