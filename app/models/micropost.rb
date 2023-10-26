006a33c5a434905f015e6dcf8943f6bb4ee4d729
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-26 09:48:11 +0900

    13.3.5演習No.1の「無効な送信」の実装

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
+  # 13.3.5演習No.1 無効な送信
+  # validates :content, presence: true, length: { maximum: 140 }
 end
