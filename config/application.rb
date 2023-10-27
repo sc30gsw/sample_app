54e0b41b1210c4d0a6b5adc1961076190c44aadf
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-27 10:29:16 +0900

    リスト14.37の実装

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
