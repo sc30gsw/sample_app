63a78e1f4ba858ab922de751ad35157f06de5fec
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-01 18:23:00 +0900

    コメントアウト

--- config/puma.rb
+++ config/puma.rb
@@ -24,7 +24,7 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 bind "unix:///var/www/sample_app/shared/tmp/sockets/puma.sock"
 
